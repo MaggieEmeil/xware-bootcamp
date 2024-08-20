acf4f64772cbfe9fe4c46f5221ebc81df8083e6c
MaggieEmeil <mangoooo2013@gmail.com>
2024-08-20 23:20:19 +0300

    add files

---
+++ track2/day3/day3.js
@@ -0,0 +1,87 @@
+    //6)Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+    // function total(arr=0){
+    //     let x=0
+    //     for(let i=0 ; i < arr.length ; i++){
+            
+    //         if(arr[i]===true){
+    //             x++;
+    //         }
+    //         else{
+    //            x+=0;
+    //         }
+           
+    //     }
+    //     return x;
+    // }
+    // console.log(total([true,false, true,true,false]));
+
+
+
+
+       //7)Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse). ex 1 = -1
+
+    //  function opposite(x=0)
+    //  {
+    //      let num = -(x);
+    //     return num;
+    //  }
+    //  console.log(opposite(-10));
+
+
+
+
+
+       //8)In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+        
+       
+    //    function opposite(x=0){
+    //     let num;
+    //     if(x>0){
+    //      num=-(x);
+    //     }
+    //     else
+    //     {
+    //         num=x;
+    //     }
+    //     return num;
+    //    }
+    //    console.log(opposite(0));
+
+
+
+
+
+   //9)Write a function findNeedle() that takes an array full of junk but containing one "needle"
+    //After your function finds the needle it should return a message (as a string) that says:
+    //"found the needle at position " plus the index it found the needle, so:
+    
+    
+
+    // function findNeedle(arr){
+    //  let x=arr.includes("needle");
+    //  let i=arr.indexOf("needle");
+    //  console.log("found the needle at position :"+ i);
+    
+    // }
+    // findNeedle(["hello","world","bye","hii","needle"]);
+      
+    
+    
+    
+    //  10)Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+    //  function result(arr=0){
+    //     let x=0;
+    //     let y=0;
+    //   for(let i=0;i<arr.length;i++)
+       
+    //     if(arr[i]>0){
+    //         x++;
+    //     }
+    //     else{
+    //         y+=arr[i];
+    //     }
+    //     return [x,y];
+    //  }
+    //     console.log(result([2,-1,2,0,-3]));
+
